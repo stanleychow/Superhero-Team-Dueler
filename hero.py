cba166eba5d986190e8e890a16b9d70c896d8ccc
stanleychow <chowtech1022@gmail.com>
2020-11-25 14:06:37 -0500

    Update hero.py
    Added Fight method

--- hero.py
+++ hero.py
@@ -1,10 +1,16 @@
+#Hero Constructor
 class Hero():
     def __init__(self, name, starting_health = 100):
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
 
+#Grace Hopper Constructor
 if __name__  == "__main__":
     my_hero = Hero("Grace Hopper", 200)
     print(my_hero.name)
     print(my_hero.current_health)
+
+#Fight Method
+def fight(self, opponent):
+    self.opponent = opponent
