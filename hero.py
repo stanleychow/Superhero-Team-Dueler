28c5a96008f1a6c95788aa1b172bbcd464f2d767
stanleychow <chowtech1022@gmail.com>
2020-11-25 10:15:46 -0500

    Added Hero class constructor

---
+++ hero.py
@@ -0,0 +1,5 @@
+class Hero():
+    def __init__(self, name, starting_health = 100):
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
