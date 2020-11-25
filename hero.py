fa9f4029c5c6901cf24e5e70335d9674ec051745
stanleychow <chowtech1022@gmail.com>
2020-11-25 10:33:22 -0500

    Added Grace Hopper hero constructor

--- hero.py
+++ hero.py
@@ -3,3 +3,8 @@ class Hero():
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+
+if __name__  == "__main__":
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
