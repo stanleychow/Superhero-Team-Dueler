5e783925a53d9a82b2a10b3b7d08d9b5e05b261e
stanleychow <chowtech1022@gmail.com>
2020-11-26 20:41:27 -0500

    Added ability.py file and Ability constructor

---
+++ ability.py
@@ -0,0 +1,5 @@
+#Ability Constructor
+class Ability:
+    def __init__:(self, name, max_damage):
+        self.name = name 
+        self.max_damage = max.damage
--- hero.py
+++ hero.py
@@ -8,12 +8,19 @@ class Hero():
         self.starting_health = starting_health
         self.current_health = starting_health
 
-#Grace Hopper Constructor
-if __name__  == "__main__":
-    my_hero = Hero("Grace Hopper", 200)
-    print(my_hero.name)
-    print(my_hero.current_health)
-
 #Fight Method
-def fight(self, opponent):
-    
+    def fight(self, opponent):
+        heroes_list = [self, opponent]
+        print(str(random.choice(heroes_list)) + " won!")
+
+# #Grace Hopper Constructor
+# if __name__  == "__main__":
+#     my_hero = Hero("Grace Hopper", 200)
+#     print(my_hero.name)
+#     print(my_hero.current_health)
+
+if __name__ == "__main__":
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+
+    hero1.fight(hero2)
