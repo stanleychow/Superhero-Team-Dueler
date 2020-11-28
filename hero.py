e08330673838ff427097a9a76fa22d868e4611fd
stanleychow <chowtech1022@gmail.com>
2020-11-27 19:52:04 -0500

    Added Attach Method to the Hero Class Constructor

--- hero.py
+++ hero.py
@@ -7,6 +7,8 @@ import random
 #Hero Constructor
 class Hero():
     def __init__(self, name, starting_health = 100):
+        self.abilities = list()
+        self.armors = list()
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
@@ -16,14 +18,38 @@ class Hero():
         heroes_list = [self.name, opponent.name]
         print(str(random.choice(heroes_list)) + " won!")
 
-# #Grace Hopper Constructor
-# if __name__  == "__main__":
-#     my_hero = Hero("Grace Hopper", 200)
-#     print(my_hero.name)
-#     print(my_hero.current_health)
+#Add Ability Method
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+#Grace Hopper Constructor
+if __name__  == "__main__":
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
 
 if __name__ == "__main__":
     hero1 = Hero("Wonder Woman")
     hero2 = Hero("Dumbledore")
 
     hero1.fight(hero2)
+
+if __name__ == "__main__":
+    ability = Ability("Great Debugging", 50)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    print(hero.abilities)
+
+if __name__ == "__main__":
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
