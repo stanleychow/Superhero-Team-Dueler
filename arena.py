9e12d24bf716037374259cd1aef0fe524063f4da
stanleychow <chowtech1022@gmail.com>
2020-11-29 20:52:46 -0500

    Added last test

--- arena.py
+++ arena.py
@@ -109,4 +109,27 @@ if __name__ == "__main__":
     arena.team_battle()
     arena.show_stats()
 
-   
+if __name__ == "__main__":
+    game_is_running = True
+
+    # Instantiate Game Arena
+    arena = Arena()
+
+    #Build Teams
+    arena.build_team_one()
+    arena.build_team_two()
+
+    while game_is_running:
+
+        arena.team_battle()
+        arena.show_stats()
+        play_again = input("Play Again? Y or N: ")
+
+        #Check for Player Input
+        if play_again.lower() == "n":
+            game_is_running = False
+
+        else:
+            #Revive heroes to play again
+            arena.team_one.revive_heroes()
+            arena.team_two.revive_heroes()
